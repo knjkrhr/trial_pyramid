60ba0a981042aab5860e3e2f11c1384e31a7edf5
KenjiKURIHARA <kurihara.kenji@donuts.ne.jp>
2019-04-26 14:34:12 +0900

    hello worldt追加

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def hello_world(request):
+    return Response('Hello World!')
+
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world,
+                        route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server(host='0.0.0.0',
+                         port=6543,
+                         app=app)
+    server.serve_forever()
